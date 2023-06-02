7a6081efb53ab0f68d0b57c06e1a011e85c7cefb
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-06-01 19:03:26 -0500

    Verificando el ganador

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,42 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  const [winner, setWinner] = useState(null);
+
+  const checktWinner = (boardToCheck) => {
+    // revisar combinaciones ganadoras para ver si x u o gano
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
   const updateBoard = (index) => {
     // no actualizar posicion existente
-    if (board[index]) return;
+    if (board[index] || winner) return;
     // actualizar el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -36,6 +64,12 @@ function App() {
     // cambia el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    // revisar ganador
+    const newWinner = checktWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+      // alert(`El ganador es ${newWinner}`)
+    }
   };
 
   return (
